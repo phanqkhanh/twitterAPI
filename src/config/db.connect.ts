f97b5459132fecd44dfb326aea6f9ff6dc7702d2
khanh <phanquockhanh9196@gmail.com>
2023-08-08 10:16:42 +0700

    register + jwt

--- src/config/db.connect.ts
+++ src/config/db.connect.ts
@@ -1,20 +1,27 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Db, Collection } from 'mongodb'
 import dotenv from 'dotenv'
+import User from '~/models/schemas/User.schema'
 
 dotenv.config()
-
-export default async function connectToDatabase() {
-  try {
-    const url = process.env.DB_CONNECTION_STRING || ''
-    const client = new MongoClient(url)
-    await client.connect()
-    console.log('Connected successfully to server')
-  } catch (error) {
-    console.log(error)
+const url = process.env.DB_CONNECTION_STRING as string
+class Database {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(url)
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+  async connect() {
+    try {
+      this.db.command({ ping: 1 })
+      console.log('Connected successfully to database')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  get users(): Collection<User> {
+    return this.db.collection('users')
   }
 }
-
-// connectToDatabase()
-//   .then(console.log)
-//   .catch(console.error)
-//   .finally(() => client.close())
+const database = new Database()
+export default database
--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,10 +1,24 @@
-import { Request, Response } from "express"
+import { Request, Response } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { RegisterReqBody } from '~/models/requests/User.requests'
+import usersService from '~/services/users.services'
 const getUser = (req: Request, res: Response) => {
 
 }
 
-const registerUser = (req: Request, res: Response) => {
-
+const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
+  try {
+    const result = await usersService.register(req.body)
+    return res.status(200).json({
+      msg: 'Success',
+      result
+    })
+  } catch (error) {
+    return res.status(200).json({
+      msg: 'Error',
+      error
+    })
+  }
 }
 
-export { getUser, registerUser }
+export { getUser, registerController }
